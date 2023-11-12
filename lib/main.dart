ac46c48ef71aa2fa5ffc803137f474d3254a526d
xcheche <chekwubechukwu23@gmail.com>
2023-11-12 16:38:21 +0100

    app completed

--- lib/main.dart
+++ lib/main.dart
@@ -15,31 +15,69 @@ class MyApp extends StatelessWidget {
           title: const Text('Hello World Travel'),
           centerTitle: true,
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Text(
-                'Hello World Travel',
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue[800]),
-              ),
-              const Text(
-                'Discover the World',
-                style: TextStyle(fontSize: 20, color: Colors.deepPurpleAccent),
-              ),
-              Image.network('https://www.itying.com/images/flutter/2.png',
-                  width: 200, height: 200),
-              TextButton(
-                onPressed: () => true,
-                child: const Text('Contact  Us'),
-              ),
-            ],
-          ),
-        ),
+        body: Builder(
+            builder: (context) => Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            'Hello World Travel',
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue[800]),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Text(
+                            'Discover the World',
+                            style: TextStyle(
+                                fontSize: 20, color: Colors.deepPurpleAccent),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Image.network(
+                              'https://www.itying.com/images/flutter/2.png',
+                              width: 200,
+                              height: 200),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: TextButton(
+                            onPressed: () => contactUs(context),
+                            child: const Text('Contact  Us'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
       ),
     );
   }
+}
+
+void contactUs(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Contact Us'),
+        content: const Text('Mail us at chechechec@.com'),
+        actions: <Widget>[
+          TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              })
+        ],
+      );
+    },
+  );
 }
